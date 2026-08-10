394eda446fbccaea42b79dbe2f9fa041a5b289cc
Xhack <72313282+energykid@users.noreply.github.com>
2026-08-10 11:06:41 -0400

    Whetstone

--- Content/Underground/DeepCaveLoot/Whetstone.cs
+++ Content/Underground/DeepCaveLoot/Whetstone.cs
@@ -1,49 +1,171 @@
-﻿using Everware.Content.Base.Items;
+﻿using Everware.Common.Systems;
+using Everware.Content.Base.Items;
+using Everware.Content.Base.ParticleSystem;
 using Everware.Utils;
 using Terraria.ID;
 
 namespace Everware.Content.Underground.DeepCaveLoot;
 
-public class Whetstone : EverPlaceableItem
+public class Whetstone : EverItem
 {
+    public override int Rarity => ItemRarityID.Pink;
+    public static readonly float ParryRange = 1.5f;
+    public static readonly int ParryDamage = 38;
     public override string Texture => "Everware/Assets/Textures/Underground/DeepCaveLoot/Whetstone";
 
     public override void SetDefaults()
     {
         base.SetDefaults();
-        Item.createTile = -1;
-        Item.maxStack = 1;
-        Item.consumable = true;
+        Item.DefaultToAccessory(32, 20);
     }
 
-    public override bool ConsumeItem(Player player)
+    public override void UpdateAccessory(Player player, bool hideVisual)
     {
-        Point p = (player.NetworkHandler().MousePosition / 16).ToPoint();
-        if (Main.tile[p].TileType == TileID.SharpeningStation)
-        {
-            while (Main.tile[p].TileFrameX > 0) p.X--;
-            while (Main.tile[p].TileFrameY > 0) p.Y--;
+        base.UpdateAccessory(player, hideVisual);
+        player.GetModPlayer<WhetstonePlayer>().active = true;
+    }
+}
 
-            for (int i = 0; i < 3; i++)
+public class RazorsEdgeBuff : ModBuff
+{
+    public override string Texture => "Everware/Assets/Textures/Underground/DeepCaveLoot/RazorsEdgeBuff";
+}
+public class RazorsEdgeCooldownBuff : ModBuff
+{
+    public override string Texture => "Everware/Assets/Textures/Underground/DeepCaveLoot/RazorsEdgeCooldownBuff";
+}
+
+public class WhetstonePlayer : ModPlayer
+{
+    public bool active = false;
+    public bool CanParry => active && !Player.HasBuff(ModContent.BuffType<RazorsEdgeCooldownBuff>());
+    public override float UseSpeedMultiplier(Item item)
+    {
+        if (Player.HasBuff(ModContent.BuffType<RazorsEdgeBuff>()) && item.DamageType == DamageClass.Melee)
+        {
+            return base.UseSpeedMultiplier(item) * 1.2f;
+        }
+
+        return base.UseSpeedMultiplier(item);
+    }
+    public override void ResetEffects()
+    {
+        active = false;
+    }
+
+    public override void PostUpdate()
+    {
+        if (CanParry && Player.ItemAnimationActive)
+        {
+            if (!Player.HasBuff(ModContent.BuffType<RazorsEdgeCooldownBuff>()))
             {
-                for (int j = 0; j < 2; j++)
+                Player.ItemCheck_GetMeleeHitbox(Player.HeldItem, Player.HeldItem.getRect(), out bool dontAttack, out Rectangle itemRect);
+
+                itemRect.X -= 10; itemRect.Y -= 10;
+                itemRect.Width += 20; itemRect.Height += 20;
+
+                // true melee parrying
+                for (int i = 0; i < Main.projectile.Length; i++)
                 {
-                    Main.tile[p.X + i, p.Y + j].ResetToType(TileID.Dirt);
+                    Projectile proj = Main.projectile[i];
+                    if (itemRect.Contains(proj.Hitbox.Center))
+                    {
+                        ParryProjectile(proj);
+                    }
+                }
+
+                // holdout projectile parrying
+                for (int i = 0; i < Main.projectile.Length; i++)
+                {
+                    Projectile proj = Main.projectile[i];
+                    if (proj != null)
+                    {
+                        // any melee projectile can deflect, it just has to be close to the Player to work
+                        if (proj.DamageType == DamageClass.Melee && proj.Distance(Player.Center) < proj.Size.Length() * Whetstone.ParryRange && proj.damage > 0)
+                        {
+                            for (int j = 0; j < Main.projectile.Length; j++)
+                            {
+                                Projectile parried = Main.projectile[j];
+                                if (parried.Hitbox.Contains(proj.Hitbox.Center))
+                                    ParryProjectile(parried);
+                            }
+                        }
+                    }
                 }
             }
-
-            SoundEngine.PlaySound(SoundID.Tink);
         }
-        return true;
     }
-
-    public override bool CanUseItem(Player player)
+    public void ParryProjectile(Projectile parried)
     {
-        Point p = (player.NetworkHandler().MousePosition / 16).ToPoint();
-        if (Main.tile[p].TileType == TileID.SharpeningStation)
+        // parried projectile needs to be small to work
+        if (parried.Size.Length() < 70 && parried.hostile && !parried.reflected && parried.active)
         {
-            return true;
+            SoundEngine.PlaySound(SoundID.Tink.WithPitchOffset(-0.6f), parried.Center);
+            SoundEngine.PlaySound(Assets.Sounds.Gear.Accessory.WhetstoneParry.Asset, parried.Center);
+            ScreenEffects.AddScreenShake(parried.Center, 4f);
+            parried.hostile = false;
+            parried.damage = Whetstone.ParryDamage;
+            parried.friendly = true;
+            parried.Center = Player.Center + new Vector2(35, 0).RotatedBy(Player.AngleTo(Player.NetworkHandler().MousePosition));
+            parried.velocity = new Vector2(10 + (parried.velocity.Length() / 2), 0).RotatedBy(Player.AngleTo(Player.NetworkHandler().MousePosition));
+            parried.reflected = true;
+            parried.extraUpdates += 1;
+            Player.AddBuff(ModContent.BuffType<RazorsEdgeBuff>(), 60 * 6);
+            Player.AddBuff(ModContent.BuffType<RazorsEdgeCooldownBuff>(), 60 * 16);
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i < 2)
+                    new ParryFlash(parried.Center, Vector2.Zero).Spawn();
+                new ParrySpark(parried.Center, new Vector2(Main.rand.NextFloat(7f), 0).RotatedByRandom(MathHelper.TwoPi)).Spawn();
+            }
         }
-        else return false;
+    }
+}
+
+public class ParrySpark : Particle
+{
+    public override Asset<Texture2D> Texture => Assets.Textures.Underground.DeepCaveLoot.ParrySpark.Asset;
+    public ParrySpark(Vector2 pos, Vector2 vel) : base(pos, vel, Vector2.One, null, null)
+    {
+        position = pos;
+        velocity = vel;
+        AffectedByLight = false;
+        Pixelated = true;
+        Rotation = velocity.AngleFrom(Vector2.Zero);
+    }
+    public override void Update()
+    {
+        Scale = new Vector2(velocity.Length() / 2f, 1f) * Opacity;
+        Rotation = velocity.AngleFrom(Vector2.Zero);
+        velocity.Y += 0.2f;
+        Opacity -= 0.08f;
+        Lighting.AddLight(position, 0.03f * Opacity, 0.05f * Opacity, 0.1f * Opacity);
+        if (Opacity < 0) Kill();
+        base.Update();
+    }
+}
+public class ParryFlash : Particle
+{
+    public override Asset<Texture2D> Texture => Assets.Textures.Underground.DeepCaveLoot.ParryFlash.Asset;
+    public ParryFlash(Vector2 pos, Vector2 vel) : base(pos, vel, Vector2.One, null, null)
+    {
+        FrameCount = new Vector2(4, 1);
+        FrameNum.X = 0;
+        position = pos;
+        velocity = vel;
+        AffectedByLight = false;
+        Pixelated = true;
+        Rotation = Main.rand.NextFloat(MathHelper.TwoPi);
+    }
+    public override void Update()
+    {
+        FrameNum.X += 0.3f;
+        if (FrameNum.X >= 4) Kill();
+        Rotation += 0.05f;
+        float sc = MathHelper.Lerp(1f, 0f, FrameNum.X / 4f);
+
+        Lighting.AddLight(position, 0.1f * sc, 0.2f * sc, 0.4f * sc);
+        base.Update();
     }
 }
