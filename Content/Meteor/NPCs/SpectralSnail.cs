50105fa0bdc16abfed2d84e357827c24f5669b90
Xhack <72313282+energykid@users.noreply.github.com>
2026-09-21 21:16:55 -0400

    spectral snail behavior

--- Content/Meteor/NPCs/SpectralSnail.cs
+++ Content/Meteor/NPCs/SpectralSnail.cs
@@ -6,6 +6,11 @@ namespace Everware.Content.Meteor.NPCs;
 
 public class SpectralSnail : EverNPC
 {
+    public enum BehaviorState
+    {
+        Wandering,
+        PhasingOut
+    }
     public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
     {
         bestiaryEntry.AddTags(
@@ -36,7 +41,7 @@ public class SpectralSnail : EverNPC
         NPC.noTileCollide = true;
         NPC.HitSound = SoundID.NPCHit1;
         NPC.DeathSound = SoundID.NPCDeath1;
-        NPC.value = CoinValue.Silver(50);
+        NPC.value = CoinValue.Silver(10);
         NPC.aiStyle = -1;
         NPC.knockBackResist = 0f;
     }
@@ -64,13 +69,9 @@ public class SpectralSnail : EverNPC
         }
     }
 
-    //Flips every 3000 ticks, probably will need to be adjusted depending on the desired behavior.
-
-    int fliptimer = 0;
-
     public override void AI()
     {
-        fliptimer++;
+        NPC.ai[0]++;
 
         NPC.velocity.X = 0.5f;
         NPC.velocity.Y = 0f;
@@ -78,20 +79,44 @@ public class SpectralSnail : EverNPC
         NPC.direction = MathF.Sign(NPC.velocity.X);
         NPC.spriteDirection = NPC.direction;
 
-
-        if (fliptimer > 3000)
+        if (NPC.ai[0] > 3000)
         {
             NPC.velocity.X = -0.5f;
             NPC.direction = MathF.Sign(NPC.velocity.X);
             NPC.spriteDirection = NPC.direction;
         }
 
-        if (fliptimer > 6000)
+        if (NPC.ai[0] > 6000)
         {
-            fliptimer = 0;
+            NPC.ai[0] = 0;
         }
 
+        if (State == (int)BehaviorState.PhasingOut)
+        {
+            NPC.velocity.X *= 1.5f + (NPC.ai[1] + NPC.ai[2]);
+            NPC.ai[1] += 0.005f;
+            if (NPC.ai[1] > 0.5f)
+            {
+                NPC.ai[2] += 0.01f;
+            }
+            if (NPC.ai[2] > 1f)
+            {
+                NPC.active = false;
+            }
+        }
     }
+
+    public override void HitEffect(NPC.HitInfo hit)
+    {
+        if (NPC.life < NPC.lifeMax * 0.9f && State == (int)BehaviorState.Wandering)
+        {
+            State = (int)BehaviorState.PhasingOut;
+            NPC.netUpdate = true;
+        }
+
+        base.HitEffect(hit);
+    }
+
     public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
     {
         var asset = Assets.Textures.Meteor.NPCs.SpectralSnail.Asset;
@@ -109,17 +134,20 @@ public class SpectralSnail : EverNPC
         Main.spriteBatch.End(out var sb);
         Main.spriteBatch.Begin(sb with { CustomEffect = shader.Shader });
         Main.EntitySpriteDraw(asset.Value, NPC.Center - screenPos, NPC.frame, drawColor, NPC.rotation, NPC.frame.Size() / 2, NPC.scale, effects);
+
         Main.spriteBatch.End();
 
-        var shader2 = Assets.Effects.Meteor.NPCs.SpectralSnailDither.CreateEffect();
-        shader2.Parameters.Resolution = asset.Size() / 2;
-        shader2.Parameters.Frames = 14;
-        shader2.Parameters.FrameNum = NPC.frame.Y / NPC.height;
-        shader2.Parameters.Progress = MathHelper.Lerp(-1f, 1f, NPC.ai[2]);
-        shader2.Apply();
-        Main.spriteBatch.Begin(sb with { CustomEffect = shader2.Shader });
+        shader.Parameters.Resolution = asset.Size() / 2;
+        shader.Parameters.Frames = 14;
+        shader.Parameters.FrameNum = NPC.frame.Y / NPC.height;
+        shader.Parameters.Progress = MathHelper.Lerp(-1f, 1f, NPC.ai[2]);
+        shader.Apply();
+
+        Main.spriteBatch.Begin(sb with { CustomEffect = shader.Shader });
 
         Main.EntitySpriteDraw(asset2.Value, NPC.Center - screenPos, NPC.frame, Color.White, NPC.rotation, NPC.frame.Size() / 2, NPC.scale, effects);
+
+        Main.spriteBatch.Restart(sb);
         return false;
     }
 }
