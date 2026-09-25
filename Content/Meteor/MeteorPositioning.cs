6a252782fa46afcad3040a5cb3d020897cb35f94
Xhack <72313282+energykid@users.noreply.github.com>
2026-09-24 23:31:56 -0400

    meteor fall animation

--- Content/Meteor/MeteorPositioning.cs
+++ Content/Meteor/MeteorPositioning.cs
@@ -1,100 +1,155 @@
 ﻿using System.Threading;
+using Everware.Common.Systems;
+using Everware.Core;
 using Everware.Utils;
 using Terraria.ID;
+using Terraria.ModLoader.IO;
 
 namespace Everware.Content.Meteor;
 
-public static class MeteorPositioning
+public class MeteorPositioning : ModSystem
 {
-    public static Point? MeteorPosition;
+    public static Point MeteorPosition = Point.Zero;
+
+    public static int MeteorAnimationTimer = 0;
+    public static bool MeteorSpawned = false;
+    
+    public override void PreWorldGen()
+    {
+        MeteorPosition = Point.Zero;
+        MeteorSpawned = false;
+    }
+    public override void OnWorldLoad()
+    {
+        MeteorAnimationTimer = 0;
+        CheckTime = -(60 * 20);
+        MeteorPosition = Point.Zero;
+        MeteorSpawned = false;
+    }
+
+    public override void SaveWorldData(TagCompound tag)
+    {
+        tag.Set("MeteorPosition", MeteorPosition.ToVector2());
+        tag.Set("MeteorSpawned", MeteorSpawned);
+    }
+
+    public override void LoadWorldData(TagCompound tag)
+    {
+        MeteorPosition = tag.Get<Vector2>("MeteorPosition").ToPoint();
+        MeteorSpawned = tag.Get<bool>("MeteorSpawned");
+    }
 
     private static int Threshold = 400;
     private static int MaxThreshold = 400;
 
-    private static int CheckTime = 0;
+    private static int CheckTime;
     
     [ModSystemHooks.PostUpdateEverything]
     public static void UpdateMeteorPosition()
     {
-        if (NPC.downedBoss2 && MeteorPosition == null)
+        if (MeteorPosition == Point.Zero)
         {
-            CheckTime--;
-
-            if (CheckTime <= 0)
+            if (NPC.downedBoss2)
             {
-                if (Main.netMode == NetmodeID.SinglePlayer)
-                {
-                    if (Main.LocalPlayer.Distance(new Vector2(Main.spawnTileX, Main.spawnTileY) * 16) < 3000
-                        || Main.LocalPlayer.position.Y > Main.worldSurface)
-                        FindPosition();
-                }
-                else if (Main.netMode == NetmodeID.Server)
-                {
-                    bool shouldCheck = true;
-                    foreach (Player player in Main.player)
-                    {
-                        if (player.active
-                            && player.Distance(new Vector2(Main.spawnTileX, Main.spawnTileY) * 16) > 5000
-                            && player.position.Y < Main.worldSurface)
-                            shouldCheck = false;
-                    }
+                CheckTime++;
 
-                    if (shouldCheck)
-                        FindPosition();
+                if (CheckTime >= 0 && CheckTime % 60 == 0)
+                {
+                    if (Main.netMode == NetmodeID.SinglePlayer)
+                    {
+                        if (Main.LocalPlayer.Distance(new Vector2(Main.spawnTileX, Main.spawnTileY) * 16) < 3000
+                            || Main.LocalPlayer.position.Y > Main.worldSurface)
+                            FindPosition();
+                    }
+                    else if (Main.netMode == NetmodeID.Server)
+                    {
+                        bool shouldCheck = true;
+                        foreach (Player player in Main.player)
+                        {
+                            if (player.active
+                                && player.Distance(new Vector2(Main.spawnTileX, Main.spawnTileY) * 16) > 5000
+                                && player.position.Y < Main.worldSurface)
+                                shouldCheck = false;
+                        }
+
+                        if (shouldCheck)
+                            FindPosition();
+                    }
                 }
+            }
+            else
+            {
+                CheckTime = -(60 * 20);
             }
         }
         else
         {
-            CheckTime = 60 * 60;
+            if (!MeteorSpawned)
+            {
+                MeteorAnimationTimer++;
+                if (MeteorAnimationTimer == 1)
+                {
+                    SoundEngine.PlaySound(Assets.Sounds.Misc.MeteorFall.Asset);
+                }
+                if (MeteorAnimationTimer == 70)
+                {
+                    SoundEngine.PlaySound(Assets.Sounds.Misc.MeteorCrash.Asset);
+                }
+                if (MeteorAnimationTimer == 78)
+                {
+                    Main.NewText(Mods.Everware.MeteorLanding.GetTextValue());
+                    MeteorGeneration.GenerateWholeSite(MeteorPosition);
+                }
+
+                if (MeteorAnimationTimer > 78 && MeteorAnimationTimer < 400)
+                {
+                    float intensity = MathHelper.Lerp(1f, 0f, (MeteorAnimationTimer - 60f) / 340f);
+                    
+                    ScreenEffects.DimScreen(intensity * 0.1f);
+                    ScreenEffects.ZoomScreen(-intensity * 0.05f);
+                    ScreenEffects.AddScreenShake(Main.LocalPlayer.Center, intensity * 10f, 0.8f);
+                }
+            }
         }
     }
+    private static readonly Thread MeteorFindThread = new(() =>
+    {
+        int tries = 100;
+        while (Threshold >= 200)
+        {
+            Point p = new Point(Main.spawnTileX, (int)Main.worldSurface - 200);
+            p.X += WorldGen.genRand.Next((int)(Main.maxTilesX * 0.2f), (int)(Main.maxTilesX * 0.4f)) *
+                   (WorldGen.genRand.NextBool() ? -1 : 1);
+            p = p.Grounded(600);
 
+            if (!IsPositionBlacklisted(p))
+            {
+                MeteorPosition = p;
+                break;
+            }
+
+            tries--;
+
+            if (tries == 0)
+            {
+                MaxThreshold = 300;
+                break;
+            }
+        }
+
+        if (Threshold < 200)
+        {
+            MaxThreshold = 300;
+        }
+
+        CheckTime = 60 * (MaxThreshold < 400 ? 60 : 30);
+    });
     public static void FindPosition()
     {
-            Thread thread = new Thread(() =>
-            {
-                int tries = 100;
-                while (Threshold >= 200)
-                {
-                    Point p = new Point(Main.spawnTileX, (int)Main.worldSurface - 200);
-                    p.X += WorldGen.genRand.Next((int)(Main.maxTilesX * 0.2f), (int)(Main.maxTilesX * 0.4f)) * (WorldGen.genRand.NextBool() ? -1 : 1);
-                    p = p.Grounded(600);
-                    
-                    Main.NewText("Meteor position not found!");
-                    if (!IsPositionBlacklisted(p))
-                    {
-                        MeteorPosition = p;
-                        // remove the next 3 lines when i'm finished testing
-                        MeteorGeneration.GenerateWholeSite(MeteorPosition.Value);
-                        Main.LocalPlayer.Teleport(MeteorPosition.Value.ToVector2() * 16 + new Vector2(0, -500));
-                        Main.NewText("Meteor position found!");
-                        break;
-                    }
-
-                    tries--;
-
-                    if (tries == 0)
-                    {
-                        MaxThreshold = 300;
-                        break;
-                    }
-                }
-
-                if (Threshold < 200)
-                {
-                    MaxThreshold = 300;
-                }
-                    
-                CheckTime = 60 * (MaxThreshold < 400 ? 60 : 30);
-            })
-            {
-                IsBackground = true,
-            };
-            thread.Start();
+        MeteorFindThread.Start();
     }
 
-    public static bool IsPositionBlacklisted(Point pt)
+    private static bool IsPositionBlacklisted(Point pt)
     {
         // if the tile at this exact spot is blacklisted, explode the rest of this logic right now
         if (Main.tile[pt].HasTile && MeteorGeneration.BlacklistedBlocks.Contains(Main.tile[pt].TileType)) return true;
